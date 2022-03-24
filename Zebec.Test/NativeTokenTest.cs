2bb670ff2716a7d93f0e5c56be83d00cb1c95167
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-03-24 14:25:39 +0545

    Add unit test for NativeToken class

--- Zebec.Test/NativeTokenTest.cs
+++ Zebec.Test/NativeTokenTest.cs
@@ -1,8 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Solnet.Wallet;
 using System;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Zebec.Models;
 using Zebec.Streams;
+using Zebec.Utils;
 
 namespace Zebec.Test
 {
@@ -13,20 +17,117 @@ namespace Zebec.Test
             "route clerk disease box emerge airport loud waste attitude film army tray " +
             "forward deal onion eight catalog surface unit card window walnut wealth medal";
 
+        private readonly Wallet testWallet = new Wallet(MnemonicWords);
+
         [TestMethod]
-        public async Task TestDepositAsync()
-        { 
-            var wallet = new Wallet(MnemonicWords);
-            Console.WriteLine(wallet.ToString());
+        public async Task TestDeposit()
+        {
+            Account testAccount = testWallet.GetAccount(0);
 
-            //var testAccount = wallet.GetAccount(10);
-            //bool isSuccessful = true;
+            ZebecResponse response = await NativeToken.Deposit(testAccount, (decimal)0.00000001d);
 
-            //var requestResult = await NativeToken.DepositAsync(testAccount, 1);
-
-            //Assert.IsTrue(isSuccessful == requestResult.WasSuccessful);
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
         }
 
-        
+        [TestMethod]
+        public async Task TestWithdraw()
+        {
+            Account testAccount = testWallet.GetAccount(0);
+
+            ZebecResponse response = await NativeToken.Withdraw(testAccount, (decimal)0.1d);
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public async Task TestInitializeStream()
+        {
+            Account senderTestAccount = testWallet.GetAccount(0);
+            Account recieverTestAccount = testWallet.GetAccount(1);
+
+            DateTime now = DateTime.UtcNow;
+
+            ZebecResponse response = await NativeToken.InitializeStream(
+                senderTestAccount,
+                recieverTestAccount,
+                1,
+                (ulong)now.ToUnixTimestamp(),
+                (ulong)now.AddMinutes(20).ToUnixTimestamp()
+                );
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public async Task TestPauseStream()
+        {
+            Account senderTestAccount = testWallet.GetAccount(0);
+            Account recieverTestAccount = testWallet.GetAccount(1);
+
+            var streamDataPda = new PublicKey("57WpV7rY9DEgWdmPxSgJ9sXey54ctNgZhEXDAKrBeqAQ");
+            ZebecResponse response = await NativeToken.PauseStream(
+                senderTestAccount,
+                recieverTestAccount,
+                streamDataPda
+                );
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public async Task TestResumeStream()
+        {
+            Account senderTestAccount = testWallet.GetAccount(0);
+            Account recieverTestAccount = testWallet.GetAccount(1);
+
+            var streamDataPda = new PublicKey("57WpV7rY9DEgWdmPxSgJ9sXey54ctNgZhEXDAKrBeqAQ");
+            ZebecResponse response = await NativeToken.ResumeStream(
+                senderTestAccount,
+                recieverTestAccount,
+                streamDataPda
+                );
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public async Task TestCancelStream()
+        {
+            Account senderTestAccount = testWallet.GetAccount(0);
+            Account recieverTestAccount = testWallet.GetAccount(1);
+
+            var streamDataPda = new PublicKey("57WpV7rY9DEgWdmPxSgJ9sXey54ctNgZhEXDAKrBeqAQ");
+            ZebecResponse response = await NativeToken.CancelStream(
+                senderTestAccount,
+                recieverTestAccount,
+                streamDataPda
+                );
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public async Task TestWithdrawFromStream()
+        {
+            Account senderTestAccount = testWallet.GetAccount(0);
+            Account recieverTestAccount = testWallet.GetAccount(1);
+
+            var streamDataPda = new PublicKey("57WpV7rY9DEgWdmPxSgJ9sXey54ctNgZhEXDAKrBeqAQ");
+            ZebecResponse response = await NativeToken.WithdrawStream(
+                senderTestAccount,
+                recieverTestAccount,
+                streamDataPda,
+                (decimal)0.000000025d
+                );
+
+            Assert.AreEqual(response.Result.HttpStatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Result.WasSuccessful);
+        }
     }
 }
