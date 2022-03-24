4c87509be40388955ae78c61fc57728f28a3dc90
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-03-24 14:16:43 +0545

    Delete Schemas.cs and rename ZebecResult class to ZebecResponse

---
+++ Zebec/Models/ZebecResponse.cs
@@ -0,0 +1,28 @@
+﻿using Solnet.Rpc.Core.Http;
+using Solnet.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zebec.Models
+{
+    public class ZebecResponse
+    {
+        public RequestResult<string> Result { get; private set; }
+
+        public PublicKey? StreamDataAddress { get; private set; }
+
+        public ZebecResponse(RequestResult<string> result)
+        {
+            Result = result;
+        }
+
+        public ZebecResponse(RequestResult<string> result, PublicKey streamDepositAccount)
+        {
+            Result = result;
+            StreamDataAddress = streamDepositAccount;
+        }
+    }
+}
