42a057a038b5758c3fa67541e77dc640ce74d6a0
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-03-27 13:21:46 +0545

    Create struct WhiteList

---
+++ Zebec/Models/WhiteList.cs
@@ -0,0 +1,10 @@
+﻿using Solnet.Wallet;
+
+namespace Zebec.Models
+{
+    public struct WhiteList
+    {
+        public PublicKey Address { get; set; }
+        public byte Counter { get; set; }
+    }
+}
