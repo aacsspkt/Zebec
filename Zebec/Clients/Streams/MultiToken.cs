46c224082e3f702c1ce411556bb9a6594543157b
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-04-01 15:56:19 +0545

    Add method: FundSol

--- Zebec/Clients/Streams/MultiToken.cs
+++ Zebec/Clients/Streams/MultiToken.cs
@@ -288,5 +288,45 @@ namespace Zebec.Clients.Streams
                 WasRequestSuccessfullyHandled = requestResult.WasRequestSuccessfullyHandled,
             };
         }
+
+
+        public static async Task<RequestResult<ZebecResponse>> FundSol(
+            Account fromAccount,
+            PublicKey streamDataPda,
+            PublicKey token,
+            ulong endTimeInUnixTimestamp,
+            ulong amount)
+        {
+            RequestResult<ResponseValue<BlockHash>> blockHash = await rpcClient.GetRecentBlockHashAsync();
+            Debug.WriteLineIf(blockHash.WasSuccessful, blockHash.Result.Value.Blockhash, "BlockHash");
+
+            byte[] transaction = new TransactionBuilder()
+                .SetRecentBlockHash(blockHash.Result.Value.Blockhash)
+                .SetFeePayer(fromAccount)
+                .AddInstruction(ZebecProgram.FundToken(
+                    fromAccount,
+                    streamDataPda,
+                    token,
+                    endTimeInUnixTimestamp,
+                    amount)
+                )
+                .Build(fromAccount);
+
+            RequestResult<string> requestResult = await rpcClient.SendTransactionAsync(transaction);
+            Debug.WriteLine(requestResult.HttpStatusCode.ToString(), nameof(requestResult.HttpStatusCode));
+            Debug.WriteLine(requestResult.WasSuccessful, nameof(requestResult.WasSuccessful));
+            Debug.WriteLine(requestResult.Reason, nameof(requestResult.Reason));
+            Debug.WriteLine(requestResult.RawRpcResponse, nameof(requestResult.RawRpcResponse));
+
+            return new RequestResult<ZebecResponse>()
+            {
+                ErrorData = requestResult.ErrorData,
+                HttpStatusCode = requestResult.HttpStatusCode,
+                Reason = requestResult.Reason,
+                Result = new ZebecResponse(requestResult.Result),
+                WasHttpRequestSuccessful = requestResult.WasHttpRequestSuccessful,
+                WasRequestSuccessfullyHandled = requestResult.WasRequestSuccessfullyHandled,
+            };
+        }
     }
 }
