1f330bcca0222c2948376eb096135096d2292e6c
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-04-04 13:09:21 +0545

    Create ResponseMaker class with function to return RequestResult<ZebecResponse> value

---
+++ Zebec/Utils/ResponseMaker.cs
@@ -0,0 +1,25 @@
+﻿using Solnet.Rpc.Core.Http;
+using Solnet.Wallet;
+using Zebec.Models;
+
+namespace Zebec.Utils
+{
+    public class ResponseMaker
+    {
+        public static RequestResult<ZebecResponse> Make(RequestResult<string> requestResult, PublicKey? streamDataAddress = null)
+        {
+            return new RequestResult<ZebecResponse>()
+            {
+                ErrorData = requestResult.ErrorData,
+                HttpStatusCode = requestResult.HttpStatusCode,
+                Reason = requestResult.Reason,
+                Result = streamDataAddress != null ?
+                    new ZebecResponse(requestResult.Result, streamDataAddress) :
+                    new ZebecResponse(requestResult.Result),
+                ServerErrorCode = requestResult.ServerErrorCode,
+                WasHttpRequestSuccessful = requestResult.WasHttpRequestSuccessful,
+                WasRequestSuccessfullyHandled = requestResult.WasRequestSuccessfullyHandled,
+            };
+        }
+    }
+}
